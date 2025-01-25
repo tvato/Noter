ea5484c4d4264ff2f5ddfb1f9f8bd2f34f90b37a
tvato <tvat0@protonmail.com>
2025-01-25 17:17:29 +0200

    fixed a item uncheck bug and deleted some obsolete code

--- components/CheckboxRow.tsx
+++ components/CheckboxRow.tsx
@@ -1,6 +1,6 @@
 import { deleteFromRows, getDBConnection, updateRowChecked, updateRowContent } from "@/db_tools/notes";
 import { useEffect, useState } from "react";
-import { Image, Pressable, SafeAreaView, StyleSheet, TextInput } from "react-native"
+import { Image, Pressable, SafeAreaView, StyleSheet, Text, TextInput } from "react-native"
 import Checkbox from "expo-checkbox"
 import Setting from "@/db_tools/Setting";
 import { Rows } from "@/constants/types";
@@ -38,19 +38,20 @@ export default function CheckboxRow(props: Props){
                 value={checked}
                 onValueChange={setChecked}
                 style={styles.checkbox}
-                color={checked ? "darkgrey" : "white"}
+                color={checked ? settings.getSettings().checkedTextColor : settings.getSettings().textColor}
             />
+            {// If item is checked it needs to be <Text> element, since <TextInput> cannot update textDecorationLine and would just throw an error
+            checked ? <Text style={[styles.checkedTextInput, { color: settings.getSettings().checkedTextColor }]} >{text}</Text> :
             <TextInput
                 style={[
-                    checked ? styles.checkedTextInput : styles.textInput,
-                    {
-                        color: checked ? settings.getSettings().checkedTextColor : settings.getSettings().textColor
-                    }
+                    styles.textInput,
+                    { color: settings.getSettings().textColor }
                 ]}
                 value={text}
                 onChangeText={setText}
                 onEndEditing={addToDatabase}
             />
+            }
             <Pressable onPress={removeRow} style={styles.pressable} >
                 <Image
                     source={settings.useBlackImage() ? require("@/assets/x_black.png") : require("@/assets/x_white.png")}
@@ -59,60 +60,9 @@ export default function CheckboxRow(props: Props){
             </Pressable>
         </SafeAreaView>
     )
-    /*
-    if(checked){
-        return (
-            <SafeAreaView style={styles.noteArea}>
-                <Checkbox
-                    value={checked}
-                    onValueChange={setChecked}
-                    style={styles.checkbox}
-                    color={"#606060"}           // CHANGE
-                />
-                <TextInput
-                    style={[styles.checkedTextInput, { color: settings.getSettings().checkedTextColor }]}
-                    value={text}
-                    onChangeText={setText}
-                    onEndEditing={addToDatabase}
-                />
-                <Pressable onPress={removeRow} style={styles.pressable} >
-                    <Image
-                        source={require("@/assets/x_white.png")}    // CHANGE to black X if background is white(ish)
-                        style={styles.image}
-                    />
-                </Pressable>
-            </SafeAreaView>
-        )
-    }
-    else{
-        return (
-            <SafeAreaView style={styles.noteArea}>
-                <Checkbox
-                    value={checked}
-                    onValueChange={setChecked}
-                    style={styles.checkbox}
-                    color={"white"}         // CHANGE
-                />
-                <TextInput
-                    style={[styles.textInput, {color: settings.getSettings().textColor}]}
-                    value={text}
-                    onChangeText={setText}
-                    onEndEditing={addToDatabase}
-                />
-                <Pressable onPress={removeRow} style={styles.pressable} >
-                    <Image source={require("@/assets/x_white.png")} style={styles.image} />
-                </Pressable>
-            </SafeAreaView>
-        )
-    }
-    */
 }
 
 const styles = StyleSheet.create({
-    titleInput: {   // Obsolete?
-        fontSize: 22,
-        color: "white",
-    },
     textInput: {
         width: "70%",
         fontSize: 16,
@@ -121,13 +71,11 @@ const styles = StyleSheet.create({
         width: "70%",
         fontSize: 16,
         textDecorationLine: "line-through",
+        verticalAlign: "middle",
     },
     noteArea: {
         flexDirection: "row",
         justifyContent: "space-evenly",
-    },
-    pressableText: { // Obsolete
-        fontSize: 22,
     },
     pressable: {
         padding: 5,
